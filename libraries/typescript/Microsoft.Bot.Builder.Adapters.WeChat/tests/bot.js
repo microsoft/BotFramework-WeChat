67563782a73b12c7b1867e5143ba1fcf8031c938
Zhipeng Wang <zhiwang@microsoft.com>
2020-01-15 11:10:49 +0800

    fixed mistake import

--- libraries/typescript/Microsoft.Bot.Builder.Adapters.WeChat/tests/bot.js
+++ libraries/typescript/Microsoft.Bot.Builder.Adapters.WeChat/tests/bot.js
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const { ActivityHandler } = require('botbuilder');
+const { ActivityHandler } = require('botbuilder-core');
 
 class EchoBot extends ActivityHandler {
     constructor() {
