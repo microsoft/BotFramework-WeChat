c89213ea2c04fee5489c79e0cc4cca28c05b0271
Zhipeng Wang <zhiwang@microsoft.com>
2020-03-02 18:25:36 +0800

    Add query user's basic information feature

---
+++ libraries/csharp_dotnetcore/Microsoft.Bot.Builder.Adapters.WeChat/Schema/UserBasicInfoList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Bot.Builder.Adapters.WeChat.Schema.JsonResults;
+using Newtonsoft.Json;
+
+namespace Microsoft.Bot.Builder.Adapters.WeChat.Schema
+{
+    public class UserBasicInfoList : WeChatJsonResult
+    {
+        [JsonProperty("user_info_list")]
+        public List<UsersBasicInformation> UserInfoList { get; set; }
+    }
+}
---
+++ libraries/csharp_dotnetcore/Microsoft.Bot.Builder.Adapters.WeChat/Schema/UsersBasicInformation.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using Microsoft.Bot.Builder.Adapters.WeChat.Schema.JsonResults;
+using Newtonsoft.Json;
+
+namespace Microsoft.Bot.Builder.Adapters.WeChat.Schema
+{
+    public class UsersBasicInformation : WeChatJsonResult
+    {
+        [JsonProperty("subscribe")]
+        public int Subscribe { get; set; }
+
+        [JsonProperty("openid")]
+        public string OpenId { get; set; }
+
+        [JsonProperty("nickname")]
+        public string NickName { get; set; }
+
+        [JsonProperty("sex")]
+        public int Sex { get; set; }
+
+        [JsonProperty("city")]
+        public string City { get; set; }
+
+        [JsonProperty("country")]
+        public string Country { get; set; }
+
+        [JsonProperty("province")]
+        public string Province { get; set; }
+
+        [JsonProperty("language")]
+        public string Language { get; set; }
+
+        [JsonProperty("headimgurl")]
+        public string HeadImgUrl { get; set; }
+
+        [JsonProperty("subscribe_time")]
+        public long SubscribeTime { get; set; }
+
+        [JsonProperty("unionid")]
+        public string UnionId { get; set; }
+
+        [JsonProperty("remark")]
+        public string Remark { get; set; }
+
+        [JsonProperty("groupid")]
+        public int GroupId { get; set; }
+
+        [JsonProperty("tagid_list")]
+        public List<int> TagIdList { get; set; }
+
+        [JsonProperty("subscribe_scene")]
+        public string SubscribeScene { get; set; }
+
+        [JsonProperty("qr_scene")]
+        public long QRScene { get; set; }
+
+        [JsonProperty("qr_scene_str")]
+        public string QRSceneStr { get; set; }
+    }
+}
